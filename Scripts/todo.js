227dfcd270bd88054d4484e2165a722aeab137eb
sokiGit <90055900+sokiGit@users.noreply.github.com>
2024-06-09 22:22:55 +0200

    dataStorage module, todos reodreding after one is removed (broken)

--- Scripts/todo.js
+++ Scripts/todo.js
@@ -1,10 +1,13 @@
+import "./dataStorage.mjs";
+import { cache, retrieveTable, saveTable } from "./dataStorage.mjs";
+
 const input = document.getElementById("add-input");
 const add_button = document.getElementById("add-button");
 const notes = document.getElementById("notes");
 
-var notes_cache = {};
+const DATA_KEY = "TODO_NOTES";
 
-console.log(input)
+retrieveTable(DATA_KEY); // initial table retireval to fill the cache
 
 add_button.addEventListener("click", (e) => {
     e.preventDefault();
@@ -31,7 +34,7 @@ function createNote(body/*string*/, id/*int*/, finished) {
     finished = finished == undefined && false || finished;
     let note = document.createElement("div");
     note.classList.add("note");
-    note.id = "note-"+id
+    note.id = `note-${id}`
     note.dataset.finished = finished && "1" || 0;
 
     let note_body = document.createElement("div");
@@ -46,18 +49,14 @@ function createNote(body/*string*/, id/*int*/, finished) {
     note_controls.classList.add("note-finish");
     note_finish.innerHTML = `<svg class="icon check"></svg>`;
     note_finish.addEventListener("click", (e) => {
-        e.preventDefault();
-        let tmp_notes_cache = localStorage.getItem("TODO_NOTES");
-        console.log(tmp_notes_cache);
-        notes_cache = tmp_notes_cache != null && JSON.parse(tmp_notes_cache) || {};
         if (note.dataset.finished == "1") {
             note.dataset.finished = "0";
-            notes_cache[id].finished = false;
+            cache[DATA_KEY][id].finished = false;
         } else {
             note.dataset.finished = "1";
-            notes_cache[id].finished = true;
+            cache[DATA_KEY][id].finished = true;
         }
-        localStorage.setItem("TODO_NOTES", JSON.stringify(notes_cache));
+        saveTable(DATA_KEY);
     })
     note_controls.appendChild(note_finish);
     
@@ -65,49 +64,38 @@ function createNote(body/*string*/, id/*int*/, finished) {
     note_controls.classList.add("note-rmeove");
     note_remove.innerHTML = `<svg class="icon delete"></svg>`;
     note_remove.addEventListener("click", (e) => {
-        e.preventDefault();
-        let tmp_notes_cache = localStorage.getItem("TODO_NOTES");
-        console.log(tmp_notes_cache);
-        notes_cache = tmp_notes_cache != null && JSON.parse(tmp_notes_cache) || {};
-        console.log(notes_cache);
-        delete notes_cache[id]; /* huh??? what is this sorcery */
-        console.log(notes_cache);
-        localStorage.setItem("TODO_NOTES", JSON.stringify(notes_cache));
+        //delete cache[DATA_KEY][id]; /* huh??? what is this sorcery */
+        let len = Object.keys(cache[DATA_KEY]).length + 1;
+        console.log("Removing data at index "+id+", length = "+len);
+        for (let i = id+1; i <= len; i++) {
+            console.log("Iterating: "+i+" out of "+len);
+            cache[DATA_KEY][i-1] = cache[DATA_KEY][i];
+            if (i == len) {
+                console.log("Removing last key! ("+i+")");
+                delete cache[DATA_KEY][i];
+            }
+        }
+        saveTable(DATA_KEY);
         note.remove();
     })
-    note_controls.appendChild(note_remove);
-    
-    note.appendChild(note_controls);
 
+    note_controls.appendChild(note_remove);
+    note.appendChild(note_controls);
     notes.appendChild(note);
 }
 
 function saveNewNote(body) {
-    let tmp_notes_cache = localStorage.getItem("TODO_NOTES");
-    console.log(tmp_notes_cache)
-    notes_cache = tmp_notes_cache != null && JSON.parse(tmp_notes_cache) || {};
-    console.log(notes_cache)
     let highest_index = 0;
-    for(let [key, _] of Object.entries(notes_cache)) if (highest_index < key) highest_index = key;
-    console.log("Highest index: "+highest_index);
+    for(let [key, _] of Object.entries(cache[DATA_KEY])) if (highest_index < key) highest_index = key;
     
     let note_index = 1+parseInt(highest_index);
     
-    notes_cache[note_index] = {"body": body, "finished": false};
+    cache[DATA_KEY][note_index] = {"body": body, "finished": false};
     
-    console.log(notes_cache)
-    console.log("saving: "+JSON.stringify(notes_cache));
-
-    localStorage.setItem("TODO_NOTES", JSON.stringify(notes_cache));
-
+    saveTable(DATA_KEY);
     return note_index;
 }
 
-let tmp_notes_cache = localStorage.getItem("TODO_NOTES");
-console.log(tmp_notes_cache)
-notes_cache = tmp_notes_cache != null && JSON.parse(tmp_notes_cache) || {};
-
-for (let [id, data] of Object.entries(notes_cache)) {
-    console.log(data);
+for (let [id, data] of Object.entries(cache[DATA_KEY])) {
     createNote(data.body, id, data.finished);
 }
