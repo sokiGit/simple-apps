fa11e5744c0999f4bd798b7a562f7cefe1a488f8
unknown <pavel.sefl@infis.cz>
2024-06-11 08:10:52 +0200

    some timer stuff

--- Scripts/timer.js
+++ Scripts/timer.js
@@ -2,30 +2,22 @@ const timer = document.getElementById("timer");
 const start_stop = document.getElementById("start-stop");
 
 var timer_running = false;
-var remaining_time = 0; // seconds
+var remaining_time = 0; // ms
 
-var starting_time = 60; // seconds
+var starting_time = 6E4; // ms
 
-start_stop.addEventListener("click", (e) => {
+start_stop.addEventListener("click", () => {
     timer_running = !timer_running;
     if (timer_running) {
         console.log("Running");
         let start_time = Date.now();
         remaining_time = starting_time;
         console.log("Prepare...");
-        let totalSeconds = 0
-        setInterval(() => {
-            totalSeconds += 1;
-            console.log(totalSeconds);
-        }, 1000)
         setInterval(() => {
             console.log("Interval");
             remaining_time -= Date.now() - start_time;
-            let ms = (remaining_time%1E3)/10;
-            let s = (remaining_time/1E3)%60;
-            let m = (remaining_time/6E4)%60;
-            let h = (remaining_time/3.6E5)%24;
-            timer.innerText = h+":"+m+":"+s+":"+ms;
+            console.log(remaining_time)
+            timer.innerText = toString(remaining_time);
         }, 100);
     }
 })
